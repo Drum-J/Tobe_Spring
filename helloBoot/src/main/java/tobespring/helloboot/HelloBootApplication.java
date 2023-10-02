e97d58599a45a0b2a02026be8b8588def5cc5ee7
DrumJ <tmdgh717@naver.com>
2023-10-02 14:55:00 +0900

    독립 실행형 스프링 애플리케이션
    팩토리 메소드 및 Bean 사용

--- helloBoot/src/main/java/tobespring/helloboot/HelloBootApplication.java
+++ helloBoot/src/main/java/tobespring/helloboot/HelloBootApplication.java
@@ -2,11 +2,14 @@ package tobespring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -17,25 +20,43 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 //스프링 부트의 도움 없이 서블릿 컨테이너 개발해보기
+@Configuration
 public class HelloBootApplication {
+
+    // 팩토리 메소드 구현
+    @Bean
+    public HelloController  helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
         //스프링 컨테이너 사용하기
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-        applicationContext.registerBean(HelloController.class); //빈 등록 완료
-        applicationContext.registerBean(SimpleHelloService.class); //빈 등록 완료
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                //톰캣(server) 띄우기
+                TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this) //dispatcherServlet  사용
+                    ).addMapping("/*");
+
+                });
+                webServer.start();
+            }
+        };
+
+        applicationContext.register(HelloBootApplication.class);
         applicationContext.refresh();//빈 오브젝트 생성
 
-        //톰캣(server) 띄우기
-        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext) //dispatcherServlet  사용
-            ).addMapping("/*");
-
-        });
-        webServer.start();
     }
 
 }
