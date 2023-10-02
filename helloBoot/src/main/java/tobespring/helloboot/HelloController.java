b086c63299b6b57ea215c4f7f7b701d03a35219f
DrumJ <tmdgh717@naver.com>
2023-10-02 15:26:26 +0900

    독립 실행형 스프링 애플리케이션 완

--- helloBoot/src/main/java/tobespring/helloboot/HelloController.java
+++ helloBoot/src/main/java/tobespring/helloboot/HelloController.java
@@ -1,22 +1,27 @@
 package tobespring.helloboot;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-//@RestController
-@RequestMapping("/hello")
+@RestController
 public class HelloController {
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println(applicationContext);
     }
 
-    @GetMapping
-    @ResponseBody
+    @GetMapping("/hello")
     public String hello(String name) {
         // SimpleHelloService helloService = new SimpleHelloService(); 직접 new 생성하지 않는다!
         return helloService.sayHello(Objects.requireNonNull(name));
---
+++ helloBoot/src/main/java/tobespring/helloboot/MySpringApplication.java
@@ -0,0 +1,31 @@
+package tobespring.helloboot;
+
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MySpringApplication {
+    public static void run(Class<?> applicationClass, String... args) {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                //톰캣(server) 띄우기
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                //dispatcherServlet.setApplicationContext(this); 없어도 됨
+
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet
+                    ).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+
+        applicationContext.register(applicationClass);
+        applicationContext.refresh();//빈 오브젝트 생성
+    }
+}
