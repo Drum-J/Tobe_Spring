e08e35f30f8427ae784a91a4832edd58ee14d191
DrumJ <tmdgh717@naver.com>
2023-10-03 14:00:32 +0900

    자동 구성 기반 애플리케이션 완

---
+++ helloBoot/src/test/java/tobyspring/study/ConfigurationTest.java
@@ -0,0 +1,70 @@
+package tobyspring.study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigurationTest {
+    @Test
+    void configuration() {
+        /*Common common = new Common();
+        assertThat(common).isSameAs(common); 이렇게 작성하면 같다.*/
+
+        /*MyConfig myConfig = new MyConfig();
+        Bean1 bean1 = myConfig.bean1();
+        Bean2 bean2 = myConfig.bean2();
+
+        assertThat(bean1.common).isSameAs(bean2.common); 이렇게 작성하면 두 common은 다르다.*/
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+        ac.register(MyConfig.class);
+        ac.refresh();
+
+        Bean1 bean1 = ac.getBean(Bean1.class);
+        Bean2 bean2 = ac.getBean(Bean2.class);
+
+        assertThat(bean1.common).isSameAs(bean2.common); // 여기선 두 common 이 같다.
+    }
+
+    @Configuration//(proxyBeanMethods = false) 이렇게 설정하면 위 test 는 실패하게 된다.
+    static class MyConfig {
+        @Bean
+        Common common() {
+            return new Common();
+        }
+
+        @Bean
+        Bean1 bean1() {
+            return new Bean1(common());
+        }
+
+        @Bean
+        Bean2 bean2() {
+            return new Bean2(common());
+        }
+    }
+
+
+    static class Bean1 {
+        private final Common common;
+
+        Bean1(Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Bean2 {
+        private final Common common;
+
+        Bean2(Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Common {
+    }
+}
